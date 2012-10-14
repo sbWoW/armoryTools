d44c1149a3ee41b0bc00bbd8f1e6e42d4e3716d4
smb <sb@sbuehl.com>
2012-10-14 23:54:12 +0200

    q&d retry code in petStalker

--- sbArmoryPetStalker.rb
+++ sbArmoryPetStalker.rb
@@ -30,21 +30,31 @@ url = URI.escape("#{base_url}?fields=pets")
 
 #resp = Net::HTTP.get_response(URI.parse(url))
 
-uri = URI.parse(url)
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
-request.initialize_http_header({"User-Agent" => useragent})
-resp = http.request(request)
-data = resp.body
+num = 1
 
-result = JSON.parse(data)
+while num < 10
 
-if result.has_key? 'Error'
-  raise "web service error"
-end
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request.initialize_http_header({"User-Agent" => useragent})
+  resp = http.request(request)
+  data = resp.body
 
-if result['status'] == "nok"
-  raise "Error: #{result['reason']} for: #{region}/#{realm}/#{character}"
+  result = JSON.parse(data)
+
+  if result.has_key? 'Error'
+    raise "web service error"
+  end
+
+  if result['status'] == "nok"
+    print "Error: #{result['reason']} for: #{region}/#{realm}/#{character}\n"
+  else
+    break
+  end
+
+  sleep (10)
+  num = num + 1
 end
 
 result['pets']['collected'].each { |d|
